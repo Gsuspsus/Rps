7dcc2a1f33b6429bf1bde0ee376cabdfadfccc13
gfalkeenthros <58170723+gfalkeenthros@users.noreply.github.com>
2020-11-23 06:01:25 +0100

    added some error handling

--- rps.hs
+++ rps.hs
@@ -1,18 +1,27 @@
 import System.Random 
+import Text.Read
 import Control.Monad
+import Data.Maybe
 import Moves
 
-humanSelect :: IO Move
-humanSelect = fmap read getLine
+
+humanSelect :: IO (Maybe Move)
+humanSelect = readMaybe <$> getLine
 
 computerSelect :: IO Move
-computerSelect = fmap toEnum (randomRIO (0,2))
+computerSelect = toEnum <$> randomRIO (0,2)
 
 main :: IO ()
 main = do
     putStrLn "Select Move"
-    playerMove <- humanSelect
+    maybeMove <- humanSelect
     computerMove <- computerSelect
+
+    when (isNothing maybeMove) $ do
+        putStrLn "Invalid move, try again"
+        main
+
+    let playerMove = fromJust maybeMove
 
     putStrLn ("You chose " ++ show playerMove)
     putStrLn ("Computer chose " ++ show computerMove)
